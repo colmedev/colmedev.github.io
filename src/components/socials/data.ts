6356db20516a4c51e8a156808e21daf85ba4aef3
colmedev <m.colmenarez.dev@gmail.com>
2024-10-22 20:24:58 -0400

    feat: :wrench: add types to the projects

---
+++ src/components/socials/data.ts
@@ -0,0 +1,22 @@
+import type { LinkItem } from '@contracts/link.types'
+
+export const socialItem: LinkItem[] = [
+  {
+    title: 'Github',
+    url: 'https://github.com/colmedev',
+    label: 'github',
+    icon: 'github'
+  },
+  {
+    title: 'Linkedin',
+    url: 'https://www.linkedin.com/in/colmedev/',
+    label: 'linkedin',
+    icon: 'linkedin'
+  },
+  {
+    title: 'mail',
+    url: 'mailto:m.colmenarez.dev@gmail.com',
+    label: 'mail',
+    icon: 'mail'
+  }
+]
---
+++ src/contracts/link.types.ts
@@ -0,0 +1,6 @@
+export interface LinkItem {
+  title: string
+  url: string
+  label: string
+  icon: string
+}
